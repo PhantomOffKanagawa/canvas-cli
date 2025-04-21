d638ac62c2c2035bbc16bbdc2dd623c4dee121ee
PhantomOffKanagawa <harry@surmafamily.com>
2025-04-20 19:58:59 -0500

    Add requirement to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="canvas-cli",
     version="0.1.0",
-    packages=find_packages(),    install_requires=[
+    packages=find_packages(),
+    install_requires=[
         "requests>=2.25.0",
     ],
     entry_points={
@@ -16,7 +17,9 @@ setup(
     url="https://github.com/PhantomOffKanagawa/canvas-cli",
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: GPL v3 License",
         "Operating System :: OS Independent",
     ],
+    python_requires=">=3.9",
 )
